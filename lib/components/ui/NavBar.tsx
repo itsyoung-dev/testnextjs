c4e0496727df7b7c8bbea9ad2c7cf65f7c71a686
Kaj <kajvdm@gmail.com>
2023-12-15 18:29:29 +0100

    refactor: navbar scroll event

--- lib/components/ui/NavBar.tsx
+++ lib/components/ui/NavBar.tsx
@@ -1,19 +1,41 @@
+"use client";
 import { NavLinks } from "@/lib/scripts/constants";
 import Image from "next/image";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const NavBar = () => {
+    const [showNavbar, setShowNavbar] = useState(true);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 0) {
+                setShowNavbar(false);
+            } else {
+                setShowNavbar(true);
+            }
+        };
+
+        window.addEventListener("scroll", handleScroll);
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
     return (
-        <header className="py-8">
-            <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <header
+            className={`py-8 fixed w-full top-0 ${
+                showNavbar ? "" : "bg-black/30"
+            } duration-200 ease-in-out`}
+        >
+            <div className="mx-auto max-w-[1440px] px-4 sm:px-6 lg:px-8">
                 <nav className="relative z-50 flex justify-between">
                     <div className="flex items-center md:gap-x-12">
                         <a href="">
                             <Image
                                 src={"/assets/tns_logo_white_transparent.png"}
                                 alt="Logo"
-                                width={32}
-                                height={32}
+                                width={35}
+                                height={35}
                             />
                         </a>
                         <div className="hidden md:flex md:gap-x-5">
