e9a410e1b49d6a934ed22e9c99ed8e8f3620ab2c
Kaj <kajvdm@gmail.com>
2023-12-17 17:02:17 +0100

    style: affiliate program styles

--- app/affiliate-program/page.tsx
+++ app/affiliate-program/page.tsx
@@ -1,10 +1,12 @@
 import AffiliateBanner from "@/lib/components/homepage/AffiliateBanner";
+import CenteredFeatures from "@/lib/components/homepage/CenteredFeatures";
 import React from "react";
 
 const AffiliatePage = () => {
     return (
         <>
             <AffiliateBanner />
+            <CenteredFeatures />
         </>
     );
 };
