e90b77966487f938183f019aba6942d877d7444f
Kaj <kajvdm@gmail.com>
2023-12-18 07:51:00 +0100

    feat: more work on affiliate marketing program box

--- lib/components/homepage/CenteredFeatures.tsx
+++ lib/components/homepage/CenteredFeatures.tsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { CheckCircle, CheckCircleOutline } from "heroicons-react";
+import PrimaryLink from "../ui/PrimaryLink";
+import SecondaryLink from "../ui/SecondaryLink";
 
 const CenteredFeatures = () => {
     return (
@@ -18,7 +21,45 @@ const CenteredFeatures = () => {
                         cursus mauris.
                     </p>
                 </div>
-                <div className="w-1/2 mt-11 h-96 bg-black border border-white/10 rounded-md "></div>
+                <div className="w-1/2 mt-11 h-96 bg-black border border-white/10 rounded-md">
+                    <div className="h-full flex flex-col justify-center px-10 w-[40%]">
+                        <h3 className="text-primary-white leading-header text-left font-semibold text-xl">
+                            Affiliate Marketing Program
+                        </h3>
+                        <p className="text-secondary-gray w-full leading-text-default tracking-text-default mt-3">
+                            Lorem ipsum dolor sit amet consectetur. Purus augue
+                            mauris porttitor et. Et sapien sit arcu pharetra
+                            quis donec auctor vulputate sit.
+                        </p>
+
+                        <div className="flex justify-start mt-6 gap-x-3">
+                            <CheckCircle className="text-primary-white" />
+                            <p className="text-primary-white font-medium text-[13px]">
+                                15% commission per successful sale
+                            </p>
+                        </div>
+                        <div className="flex mt-2 gap-x-3">
+                            <CheckCircle className="text-primary-white" />
+                            <p className="text-primary-white font-medium text-[13px]">
+                                Access to analytics
+                            </p>
+                        </div>
+                        <div className="flex gap-x-4">
+                            <a
+                                href="/"
+                                className="w-32 h-9 rounded-md flex items-center justify-center bg-primary-white text-primary-black text-sm mt-8"
+                            >
+                                Join now
+                            </a>
+                            <a
+                                href="/"
+                                className="w-32 h-9 rounded-md flex items-center justify-center bg-primary-white/10 text-primary-white text-sm mt-8"
+                            >
+                                Learn more
+                            </a>
+                        </div>
+                    </div>
+                </div>
             </div>
         </section>
     );
