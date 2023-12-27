7ba43eeb0c8ebc14027b402a90ea0bc0ca645a4e
Kaj <kajvdm@gmail.com>
2023-12-27 04:30:19 +0100

    feat: prisma client init

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,8 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { PrismaClient } from "@prisma/client";
+
+const prisma: PrismaClient = new PrismaClient();
 
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
@@ -9,3 +12,4 @@ export function isBase64Image(imageData: string) {
     const base64Regex = /^data:image\/(png|jpe?g|gif|webp);base64,/;
     return base64Regex.test(imageData);
 }
+
