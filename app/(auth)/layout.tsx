13d6c17a98b79e09f80c7af9914199af33ef519d
Kaj <kajvdm@gmail.com>
2023-12-27 04:04:21 +0100

    fix: add default layout to auth route

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,18 +1,25 @@
-import { ClerkProvider } from '@clerk/nextjs';
-import { inter } from '@/lib/scripts/font';
-import '@/lib/styles/main.scss';
+import { ClerkProvider } from "@clerk/nextjs";
+import { inter } from "@/lib/scripts/font";
+import "@/lib/styles/main.scss";
+import NavBar from "@/lib/components/ui/NavBar";
+import Footer from "@/lib/components/footers/Footer";
 
-export default function Layout({ children, }: { children: React.ReactNode; }) {
+export const metadata = {
+    description: "The North Solution site description",
+};
+
+export default function Layout({ children }: { children: React.ReactNode }) {
     return (
         <ClerkProvider>
             <html lang="en">
                 <body className={inter.className}>
-                    <div className="h-full w-full flex items-center justify-center">
+                    <div className="h-full w-full flex flex-col justify-between">
+                        <NavBar />
                         {children}
+                        <Footer />
                     </div>
                 </body>
             </html>
         </ClerkProvider>
     );
 }
-
