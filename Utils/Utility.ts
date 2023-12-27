ce402c7b6bea4ff27ce3d415e3e9c77fed6bb35a
Kaj <kajvdm@gmail.com>
2023-12-27 05:27:43 +0100

    fix: export prisma

--- Utils/Utility.ts
+++ Utils/Utility.ts
@@ -2,7 +2,7 @@ import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 import { PrismaClient } from "@prisma/client";
 
-const prisma: PrismaClient = new PrismaClient();
+export const prisma: PrismaClient = new PrismaClient();
 
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
