bfc4d56f22d2683c7f5ca42c0b6415e845274827
Kaj <kajvdm@gmail.com>
2023-12-27 04:07:57 +0100

    chore: account profile component for onboarding

---
+++ lib/components/forms/AccountProfile.tsx
@@ -0,0 +1,210 @@
+"use client";
+
+import React, { ChangeEvent, useState } from "react";
+import { FormProvider, useForm } from "react-hook-form";
+import { Button } from "@/lib/components/ui/button";
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/lib/components/ui/form";
+import { Textarea } from "@/lib/components/ui/textarea";
+import { Input } from "@/lib/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { UserValidation } from "@/lib/validations/user";
+import * as z from "zod";
+import Image from "next/image";
+import { isBase64Image } from "@/lib/utils";
+import { useUploadThing } from "@/lib/hooks/uploadthing";
+
+interface Props {
+    user: {
+        id: string;
+        objectId: string;
+        username: string;
+        name: string;
+        bio: string;
+        image: string;
+    };
+    btnTitle: string;
+}
+
+const AccountProfile = ({ user, btnTitle }: Props) => {
+    const [files, setFiles] = useState<File[]>([]);
+    const { startUpload } = useUploadThing("media");
+
+    const form = useForm({
+        resolver: zodResolver(UserValidation),
+        defaultValues: {
+            profile_photo: user?.image || "",
+            name: user?.name || "",
+            username: user?.username || "",
+            bio: user?.bio || "",
+        },
+    });
+
+    function handleImage(
+        e: ChangeEvent<HTMLInputElement>,
+        fieldChange: (value: string) => void
+    ) {
+        e.preventDefault();
+
+        const fileReader = new FileReader();
+
+        if (e.target.files && e.target.files.length > 0) {
+            const file = e.target.files[0];
+
+            setFiles(Array.from(e.target.files));
+
+            if (!file.type.includes("image")) return;
+
+            fileReader.onload = async (event) => {
+                const imageDataUrl = event.target?.result?.toString() || "";
+
+                fieldChange(imageDataUrl);
+            };
+
+            fileReader.readAsDataURL(file);
+        }
+    }
+
+    const onSubmit = async (values: z.infer<typeof UserValidation>) => {
+        const blob = values.profile_photo;
+
+        const hasImageChanged = isBase64Image(blob);
+
+        if (hasImageChanged) {
+            const imgRes = await startUpload(files);
+
+            if (imgRes && imgRes[0].url) {
+                values.profile_photo = imgRes[0].url;
+            }
+        }
+
+        // TODO: Update user profile
+    };
+    return (
+        <Form {...form}>
+            <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="flex flex-col justify-start gap-10"
+            >
+                <FormField
+                    control={form.control}
+                    name="profile_photo"
+                    render={({ field }) => (
+                        <FormItem className="flex items-center gap-4">
+                            <FormLabel className="account-form_image-label">
+                                {field.value ? (
+                                    <Image
+                                        src={field.value}
+                                        alt="profile photo"
+                                        width={96}
+                                        height={96}
+                                        priority
+                                        className="rounded-full object-contain"
+                                    />
+                                ) : (
+                                    <Image
+                                        src={"/assets/profile.svg"}
+                                        alt="profile photo"
+                                        width={24}
+                                        height={24}
+                                        className="object-contain"
+                                    />
+                                )}
+                            </FormLabel>
+                            <FormControl className="flex-1 font-semibold text-gray-200">
+                                <Input
+                                    type="file"
+                                    accept="image/*"
+                                    placeholder="Upload a photo"
+                                    className="account-form_image-input"
+                                    onChange={(e) =>
+                                        handleImage(e, field.onChange)
+                                    }
+                                />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                    control={form.control}
+                    name="name"
+                    render={({ field }) => (
+                        <FormItem className="flex flex-col items-center gap-4">
+                            <FormLabel className="font-semibold text-white gap-3 w-full">
+                                Name
+                            </FormLabel>
+                            <FormControl className="flex-1 font-semibold text-gray-200">
+                                <Input
+                                    className="account-form_input outline-none"
+                                    {...field}
+                                    type="text"
+                                />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                    control={form.control}
+                    name="username"
+                    render={({ field }) => (
+                        <FormItem className="flex flex-col items-center gap-4">
+                            <FormLabel className="font-semibold text-white gap-3 w-full">
+                                Username
+                            </FormLabel>
+                            <FormControl className="flex-1 font-semibold text-gray-200">
+                                <Input
+                                    className="account-form_input focus:outline-none"
+                                    {...field}
+                                    type="text"
+                                />
+                            </FormControl>
+                            <FormDescription>
+                                This is your public display name.
+                            </FormDescription>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                    control={form.control}
+                    name="bio"
+                    render={({ field }) => (
+                        <FormItem className="flex flex-col items-center gap-4">
+                            <FormLabel className="font-semibold text-white gap-3 w-full">
+                                Bio
+                            </FormLabel>
+                            <FormControl className="flex-1 font-semibold text-gray-200">
+                                <Textarea
+                                    className="account-form_input no-focus"
+                                    {...field}
+                                    rows={10}
+                                />
+                            </FormControl>
+                            <FormDescription>
+                                This is your public display name.
+                            </FormDescription>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <Button
+                    type="submit"
+                    className="!bg-primary-blurple !text-white"
+                >
+                    Submit
+                </Button>
+            </form>
+        </Form>
+    );
+};
+
+export default AccountProfile;
