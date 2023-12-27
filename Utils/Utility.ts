5b568103279044ceda3a05697118ff3b682fed73
Kaj <kajvdm@gmail.com>
2023-12-27 04:45:50 +0100

    feat: new utility file

---
+++ Utils/Utility.ts
@@ -0,0 +1,15 @@
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { PrismaClient } from "@prisma/client";
+
+const prisma: PrismaClient = new PrismaClient();
+
+export function cn(...inputs: ClassValue[]) {
+    return twMerge(clsx(inputs));
+}
+
+export function isBase64Image(imageData: string) {
+    const base64Regex = /^data:image\/(png|jpe?g|gif|webp);base64,/;
+    return base64Regex.test(imageData);
+}
+
