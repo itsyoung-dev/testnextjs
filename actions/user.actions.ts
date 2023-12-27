88721ea38786d559fb0cc6050bbf9575d3c34e1f
Kaj <kajvdm@gmail.com>
2023-12-27 05:27:13 +0100

    feat: upserted update user action

--- actions/user.actions.ts
+++ actions/user.actions.ts
@@ -1,3 +1,50 @@
 "use server";
+import { prisma } from "@/Utils/Utility";
+import { revalidatePath } from "next/cache";
 
-export async function updateUser(): Promise<void> {}
+interface Params {
+    userId: string;
+    username: string;
+    name: string;
+    bio: string;
+    image: string;
+    path: string;
+}
+
+export async function updateUser({
+    userId,
+    username,
+    name,
+    bio,
+    image,
+    path,
+}: Params): Promise<void> {
+    try {
+        await prisma.user.upsert({
+            where: {
+                id: userId,
+            },
+            update: {
+                username: username.toLowerCase(),
+                name,
+                bio,
+                image,
+                onboarded: true,
+            },
+            create: {
+                id: userId,
+                username: username.toLowerCase(),
+                name,
+                bio,
+                image,
+                onboarded: true,
+            },
+        });
+
+        if (path === "/profile/edit") {
+            revalidatePath(path);
+        }
+    } catch (error: any) {
+        throw new Error(`Failed to upsert user: ${error.message}`);
+    }
+}
