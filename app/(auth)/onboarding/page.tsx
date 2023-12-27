0dfaafe68655d552d6a9caa7867e1650cae6653e
Kaj <kajvdm@gmail.com>
2023-12-27 04:08:26 +0100

    chore: unfinished onboarding page

---
+++ app/(auth)/onboarding/page.tsx
@@ -0,0 +1,33 @@
+import AccountProfile from "@/lib/components/forms/AccountProfile";
+import { currentUser } from "@clerk/nextjs";
+
+const Page = async () => {
+    const user = await currentUser();
+
+    const userInfo = {};
+
+    const userData = {
+        id: user?.id,
+        objectId: userInfo?._id,
+        username: userInfo?.username || user?.username,
+        name: userInfo?.name || userInfo?.firstName || "",
+        bio: userInfo?.bio || "",
+        image: userInfo?.image || userInfo.imageURL,
+    };
+    return (
+        <main className="flex py-48 flex-col">
+            <div className="flex flex-col max-w-3xl">
+                <h1 className="text-primary-white text-lg">Onboarding</h1>
+                <p className="mt-3 text-regular text-light-2 text-primary-white">
+                    Complete your profile now to get started.
+                </p>
+            </div>
+
+            <section className="mt-9 bg-white/10 p-10">
+                <AccountProfile user={userData} btnTitle="Continue" />
+            </section>
+        </main>
+    );
+};
+
+export default Page;
