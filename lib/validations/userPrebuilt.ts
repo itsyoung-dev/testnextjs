f062aaf40ef8b6287e9634b3feed63093b153b7e
Kaj <kajvdm@gmail.com>
2023-12-28 07:21:17 +0100

    feat: create new user validation

--- lib/validations/userPrebuilt.ts
+++ lib/validations/userPrebuilt.ts
@@ -1,13 +1,17 @@
 import * as z from "zod";
 
 export const UserValidation = z.object({
-    email: z
-        .string()
-        .min(3, { message: "MINIMUM 3 CHARACTERS" })
-        .max(30, { message: "MAXIMUM 30 CHARACTERS" })
-        .email(),
-    password: z
+    profile_photo: z.string().url().min(1),
+    name: z
         .string()
         .min(3, { message: "MINIMUM 3 CHARACTERS" })
         .max(30, { message: "MAXIMUM 30 CHARACTERS" }),
+    username: z
+        .string()
+        .min(3, { message: "MINIMUM 3 CHARACTERS" })
+        .max(30, { message: "MAXIMUM 30 CHARACTERS" }),
+    bio: z
+        .string()
+        .min(3, { message: "MINIMUM 3 CHARACTERS" })
+        .max(1000, { message: "MAXIMUM 1000 CHARACTERS" }),
 });
