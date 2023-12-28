18529bbefc0e90b8e69d2f5a9dfd7810960f7009
Kaj <kajvdm@gmail.com>
2023-12-28 18:00:50 +0100

    refactor: middleware dividings

--- middleware.ts
+++ middleware.ts
@@ -2,12 +2,16 @@ import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
     publicRoutes: [
+        // pages
         "/",
         "/earn-with-us(.*)",
         "/courses(.*)",
         "/affiliate-program(.*)",
+        // auth
         "/login(.*)",
+        "/signup(.*)",
         "/reset-password(.*)",
+        // api
         "/api/webhook/clerk",
     ],
     ignoredRoutes: ["/api/webhook/clerk"],
