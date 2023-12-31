8a37b005f2b56a894855f92b394123e4c4eac0b3
Kaj <kajvdm@gmail.com>
2023-12-31 22:31:21 +0100

    feat: dashboard button on logged in logged in state

--- components/ui/NavBar.tsx
+++ components/ui/NavBar.tsx
@@ -4,10 +4,10 @@ import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
+// import UserButton from "@/components/user/UserButton";
 
 const NavBar = () => {
     const [showNavbar, setShowNavbar] = useState(true);
-
     useEffect(() => {
         const handleScroll = () => {
             if (window.scrollY > 0) {
@@ -23,6 +23,7 @@ const NavBar = () => {
             window.removeEventListener("scroll", handleScroll);
         };
     }, []);
+
     return (
         <header
             className={`py-8 fixed w-full top-0 ${
@@ -67,35 +68,53 @@ const NavBar = () => {
                                     </a>
                                 </React.Fragment>
                             ))}
+                            <SignedIn>
+                                <div className="hidden md:block">
+                                    <a
+                                        href="/login"
+                                        className="inline-block rounded-lg px-2 py-1 text-sm text-secondary-gray hover:text-primary-white/[88%] duration-200"
+                                    >
+                                        Contact
+                                    </a>
+                                </div>
+                            </SignedIn>
                         </div>
                     </div>
                     <div className="flex items-center gap-x-5">
-                        <div className="hidden md:block">
-                            <a
-                                href="/login"
-                                className="inline-block rounded-lg px-2 py-1 text-sm text-secondary-gray hover:text-primary-white/[88%] duration-200"
-                            >
-                                Contact
-                            </a>
-                        </div>
                         <SignedOut>
                             <div className="hidden md:block">
                                 <a
                                     href="/login"
-                                    className="inline-block rounded-md px-4 py-1 text-sm text-primary-white duration-200 bg-white/10 border border-white/10 hover:border-white/20 shadow-md shadow-black/60"
+                                    className="inline-block rounded-lg px-2 py-1 text-sm text-secondary-gray hover:text-primary-white/[88%] duration-200"
                                 >
-                                    Log in
+                                    Contact
+                                </a>
+                            </div>
+                            <div className="hidden md:block">
+                                <a
+                                    href="/login"
+                                    className="flex items-center rounded-md px-4 py-[4px] text-sm text-primary-white duration-200 bg-white/10 border border-white/10 hover:border-white/20"
+                                >
+                                    <span className="pt-[2px]">Log in</span>
                                 </a>
                             </div>
                             <a
                                 href="/signup"
-                                className="group inline-flex items-center justify-center rounded-md py-1 px-4 text-sm font-normal focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 bg-primary-blurple text-primary-white hover:opacity-80 duration-200 shadow-md shadow-primary-black/60"
+                                className="flex items-center justify-center rounded-md py-[4px] px-4 text-sm font-normal focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 bg-primary-blurple text-primary-white hover:opacity-80 duration-200"
                             >
-                                <span>Sign up</span>
+                                <span className="pt-[2px]">Sign up</span>
                             </a>
                         </SignedOut>
                         <SignedIn>
-                            <UserButton afterSignOutUrl="/" />
+                            <div className="mr-1">
+                                <a
+                                    href="/dashboard"
+                                    className="flex items-end text-center bg-white/10 border border-white/10 rounded-md px-3 py-[4px] text-sm text-primary-white hover:text-primary-white/[88%] hover:border-white/20 duration-200"
+                                >
+                                    <span className="pt-[2px]">Dashboard</span>
+                                </a>
+                            </div>
+                            <UserButton />
                         </SignedIn>
                         <div className="-mr-1 md:hidden"></div>
                     </div>
