9a7e3c11cd88df5e2aa749182b42dc3fc7b71a36
Kaj <kajvdm@gmail.com>
2024-01-02 00:53:25 +0100

    refactor: merge cards, fix styling and spacing

--- components/homepage/Services.tsx
+++ components/homepage/Services.tsx
@@ -1,13 +1,22 @@
 import React from "react";
-import ServicesCard from "./ServicesCard";
 import software from "../../public/assets/software-services.png";
 import host from "../../public/assets/host-services.png";
 import web from "../../public/assets/web-services.png";
+import Image, { StaticImageData } from "next/image";
+import Link from "next/link";
+import { ArrowRightIcon } from "@heroicons/react/24/outline";
+
+interface ServiceCardProps {
+    image: StaticImageData;
+    text: string;
+    heading: string;
+    href: string;
+}
 
 function Services() {
     return (
-        <section className="w-full h-full lg:h-[844px] bg-black px-6 flex-col flex">
-            <div className="flex flex-col mt-36 mx-8 lg:px-20">
+        <section className="w-full h-full lg:h-[844px] bg-black spacing-default flex-col flex">
+            <div className="flex flex-col pt-40 pb-8">
                 <h5 className="text-[#5865F2] font-semibold pb-6  text-xs">
                     Services
                 </h5>
@@ -20,27 +29,54 @@ function Services() {
                     success.
                 </p>
             </div>
-            <div className="grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 gap-6 mt-10 mx-auto items-center justify-center">
+            <div className="grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 gap-6 mt-10 items-center justify-center">
                 <ServicesCard
                     href="/"
-                    heading="Software Services"
+                    heading="Software services"
                     text="From simple automations to sophisticated algorithms."
                     image={software}
                 />
                 <ServicesCard
                     href="/"
-                    heading="Web Services"
+                    heading="Web services"
                     text="Boost your online presence with a professional website."
                     image={web}
                 />
                 <ServicesCard
                     href="/"
-                    heading="Host Services"
+                    heading="Host services"
                     text="Keep your website or software online and protected."
                     image={host}
                 />
             </div>
         </section>
+    );
+}
+
+function ServicesCard({ image, text, heading, href }: ServiceCardProps) {
+    return (
+        <Link href={"/"}>
+            <article className="h-auto rounded-[15px] border border-primary-white/10 flex flex-col service-card">
+                <Image
+                    className="w-full h-[232px] rounded-t-[15px] object-cover"
+                    alt="service card"
+                    src={image}
+                />
+                <div className="p-4 gap-3 h-24 linear-bg">
+                    <h3 className="font-semibold text-[18px] tracking-text-default leading-text-default text-primary-white mb-1">
+                        {heading}
+                    </h3>
+                    <span className="flex justify-between">
+                        <p className="font-normal text-secondary-gray text-[13px] leading-text-default tracking-text-default w-[222px]">
+                            {text}
+                        </p>
+                        <Link href={href}>
+                            <ArrowRightIcon className="text-white w-4 h-4" />
+                        </Link>
+                    </span>
+                </div>
+            </article>
+        </Link>
     );
 }
 
