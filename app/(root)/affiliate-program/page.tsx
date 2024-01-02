3c31413f46a091d39c0d16f0c672746e6031cec2
Kaj <kajvdm@gmail.com>
2024-01-02 02:34:00 +0100

    refactor: move root site pages into (root)

---
+++ app/(root)/affiliate-program/page.tsx
@@ -0,0 +1,14 @@
+import AffiliateBanner from "@/components/homepage/AffiliateBanner";
+import CenteredFeatures from "@/components/homepage/CenteredFeatures";
+import React from "react";
+
+const AffiliatePage = () => {
+    return (
+        <>
+            <AffiliateBanner />
+            <CenteredFeatures />
+        </>
+    );
+};
+
+export default AffiliatePage;
