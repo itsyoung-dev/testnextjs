1574781cef3123889dbbcfaa2d092756e853e3d4
Kaj <kajvdm@gmail.com>
2024-01-03 02:28:16 +0100

    feat: add getUserInfoById function

--- actions/user.actions.ts
+++ actions/user.actions.ts
@@ -1,58 +1,28 @@
 "use server";
 import { prisma } from "@/utils/Utility";
-import { revalidatePath } from "next/cache";
 
 interface Params {
     userId: string;
-    username: string;
-    name: string;
-    bio: string;
-    image: string;
-    path: string;
 }
 
-export async function updateUser({
-    userId,
-    username,
-    name,
-    bio,
-    image,
-    path,
-}: Params): Promise<void> {
+export async function getUserInfoById({ userId }: Params) {
     try {
-        await prisma.user.upsert({
+        const user = await prisma.user.findFirst({
             where: {
                 id: userId,
             },
-            update: {
-                username: username.toLowerCase(),
-                name,
-                bio,
-                image,
-                onboarded: true,
-            },
-            create: {
-                id: userId,
-                username: username.toLowerCase(),
-                name,
-                bio,
-                image,
-                onboarded: true,
-            },
         });
 
-        if (path === "/profile/edit") {
-            revalidatePath(path);
-        }
-    } catch (error: any) {
-        throw new Error(`Failed to upsert user: ${error.message}`);
-    }
-}
-
-export async function fetchUser(userId: string) {
-    try {
-        return await prisma.user.findFirst({ where: { id: userId } });
+        return user;
     } catch (error: any) {
         throw new Error(`Failed to fetch user: ${error.message}`);
     }
 }
+
+// export async function fetchUser(userId: string) {
+//     try {
+//         return await prisma.user.findFirst({ where: { id: userId } });
+//     } catch (error: any) {
+//         throw new Error(`Failed to fetch user: ${error.message}`);
+//     }
+// }
