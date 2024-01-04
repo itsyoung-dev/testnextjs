ddb10d003f08d79f66e889669eeee9065c1d9b20
Kaj <kajvdm@gmail.com>
2024-01-04 23:57:53 +0100

    fix: run user creation on server action

--- actions/user.actions.ts
+++ actions/user.actions.ts
@@ -1,11 +1,16 @@
 "use server";
 import { prisma } from "@/utils/Utility";
 
-interface Params {
+interface SearchParams {
     userId: string;
 }
 
-export async function getUserInfoById({ userId }: Params) {
+interface CreationParams {
+    userId: string;
+    options: object;
+}
+
+export async function getUserInfoById({ userId }: SearchParams) {
     try {
         const user = await prisma.user.findFirst({
             where: {
@@ -14,6 +19,32 @@ export async function getUserInfoById({ userId }: Params) {
         });
 
         return user;
+    } catch (error: any) {
+        throw new Error(`Failed to fetch user: ${error.message}`);
+    }
+}
+
+export async function createUser({ userId, options }: CreationParams) {
+    try {
+        const user = await prisma.user.create({
+            data: {
+                id: userId,
+            },
+        });
+
+        return user;
+    } catch (error: any) {
+        throw new Error(`Failed to fetch user: ${error.message}`);
+    }
+}
+
+export async function deleteUser({ userId }: SearchParams) {
+    try {
+        await prisma.user.delete({
+            where: {
+                id: userId,
+            },
+        });
     } catch (error: any) {
         throw new Error(`Failed to fetch user: ${error.message}`);
     }
