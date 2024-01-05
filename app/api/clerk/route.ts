af098f7b5e0bedf54ed30f6573e7b68517f6b2ab
Kaj <kajvdm@gmail.com>
2024-01-05 02:55:14 +0100

    fix: properly verify webhook signature

--- app/api/clerk/route.ts
+++ app/api/clerk/route.ts
@@ -1,61 +1,63 @@
 import { deleteUser } from "@/actions/user.actions";
 import { WebhookEvent } from "@clerk/nextjs/server";
 import { headers } from "next/headers";
-import { Webhook, WebhookRequiredHeaders } from "svix";
-import { IncomingHttpHeaders } from "http";
+import { Webhook } from "svix";
 
-type EventType = "user.deleted";
+export async function POST(req: Request) {
+    const WEBHOOK_SECRET = process.env.CLERK_WEBHOOK_SECRET;
 
-type Event = {
-    data: Record<string, string | number | Record<string, string>[]>;
-    object: "event";
-    type: EventType;
-};
-
-export async function POST(request: Request) {
-    const payload: WebhookEvent = await request.json();
-    // const header = headers();
-
-    // const heads = {
-    //     "svix-id": header.get("svix-id"),
-    //     "svix-timestamp": header.get("svix-timestamp"),
-    //     "svix-signature": header.get("svix-signature"),
-    // };
-
-    // console.log(heads);
-
-    // const wh = new Webhook(process.env.NEXT_CLERK_WEBHOOK_SECRET || "");
-
-    // let evnt: Event | null = null;
-
-    // try {
-    //     evnt = wh.verify(
-    //         JSON.stringify(payload),
-    //         heads as IncomingHttpHeaders & WebhookRequiredHeaders
-    //     ) as Event;
-
-    //     console.log(evnt);
-    // } catch (err) {
-    //     return Response.json({ message: err }, { status: 400 });
-    // }
-
-    // const eventType: EventType = evnt?.type!;
-
-    // if (eventType === "user.deleted") {
-    // const { id } = evnt?.data ?? {};
-    const id = payload.data.id;
-
-    try {
-        // @ts-ignore
-        await deleteUser({ userId: id });
-
-        return Response.json({ message: "User deleted" }, { status: 201 });
-    } catch (error) {
-        console.log(error);
-        return Response.json(
-            { message: "Internal Server Error" },
-            { status: 500 }
+    if (!WEBHOOK_SECRET) {
+        throw new Error(
+            "Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local"
         );
     }
-    // }
+
+    const headerPayload = headers();
+    const svix_id = headerPayload.get("svix-id");
+    const svix_timestamp = headerPayload.get("svix-timestamp");
+    const svix_signature = headerPayload.get("svix-signature");
+
+    if (!svix_id || !svix_timestamp || !svix_signature) {
+        return new Response("Error occured -- no svix headers", {
+            status: 400,
+        });
+    }
+
+    const payload = await req.json();
+    const body = JSON.stringify(payload);
+
+    const wh = new Webhook(WEBHOOK_SECRET);
+
+    let evt: WebhookEvent;
+
+    try {
+        evt = wh.verify(body, {
+            "svix-id": svix_id,
+            "svix-timestamp": svix_timestamp,
+            "svix-signature": svix_signature,
+        }) as WebhookEvent;
+    } catch (err) {
+        console.error("Error verifying webhook:", err);
+        return new Response("Error occured", {
+            status: 400,
+        });
+    }
+
+    const { id } = evt.data;
+    const eventType = evt.type;
+
+    if (eventType === "user.deleted") {
+        try {
+            // @ts-ignore
+            await deleteUser({ userId: id });
+
+            return Response.json({ message: "User deleted" }, { status: 201 });
+        } catch (error) {
+            console.log(error);
+            return Response.json(
+                { message: "Internal Server Error" },
+                { status: 500 }
+            );
+        }
+    }
 }
