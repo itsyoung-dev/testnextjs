a64d38b928460b1f3c0f472cfe60e504c96e352d
Kaj <kajvdm@gmail.com>
2024-01-07 09:20:22 +0100

    feat: server actions for functional signing up and signing in with custom errors

--- actions/auth.actions.ts
+++ actions/auth.actions.ts
@@ -1,8 +1,15 @@
 "use server";
 import * as z from "zod";
+import bcrypt from "bcryptjs";
 
 import { LoginValidation } from "@/lib/validations/login";
 import { SignupValidation } from "@/lib/validations/signup";
+
+import { prisma } from "@/utils/Utility";
+import { getUserByEmail } from "@/actions/user.actions";
+import { signIn } from "@/auth";
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
+import { AuthError } from "next-auth";
 
 export const login = async (values: z.infer<typeof LoginValidation>) => {
     const validatedFields = LoginValidation.safeParse(values);
@@ -11,7 +18,26 @@ export const login = async (values: z.infer<typeof LoginValidation>) => {
         return { error: "Invalid fields!" };
     }
 
-    return { success: "Email sent!" };
+    const { email, password } = validatedFields.data;
+
+    try {
+        await signIn("credentials", {
+            email,
+            password,
+            redirectTo: DEFAULT_LOGIN_REDIRECT,
+        });
+    } catch (error) {
+        if (error instanceof AuthError) {
+            switch (error.type) {
+                case "CredentialsSignin":
+                    return { error: "Invalid credentials" };
+                default:
+                    return { error: "Something went wrong" };
+            }
+        }
+
+        throw error;
+    }
 };
 
 export const signup = async (values: z.infer<typeof SignupValidation>) => {
@@ -21,5 +47,25 @@ export const signup = async (values: z.infer<typeof SignupValidation>) => {
         return { error: "Invalid fields!" };
     }
 
-    return { success: "Email sent!" };
+    const { email, password, name } = validatedFields.data;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const existingUser = await getUserByEmail({ email });
+
+    if (existingUser) {
+        return { error: "Email already in use" };
+    }
+
+    await prisma.user.create({
+        data: {
+            name,
+            email,
+            password: hashedPassword,
+        },
+    });
+
+    // TODO: Send verification token email
+    // TODO: Onboarding
+
+    return { success: "User created" };
 };
