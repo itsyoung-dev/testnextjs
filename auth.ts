100cc155f27f5dafdbcc172b846a375210fa578e
Kaj <kajvdm@gmail.com>
2024-01-07 09:45:47 +0100

    feat: add user role hierarchy and register on session

--- auth.ts
+++ auth.ts
@@ -3,12 +3,37 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 import authConfig from "@/auth.config";
 import { prisma } from "@/utils/Utility";
 
+import { getUserById } from "@/actions/user.actions";
+import { UserRole } from "@prisma/client";
+
 export const {
     handlers: { GET, POST },
     auth,
     signIn,
     signOut,
 } = NextAuth({
+    callbacks: {
+        async session({ token, session }) {
+            if (token.sub && session.user) session.user.id = token.sub;
+
+            if (token.role && session.user) {
+                session.user.role = token.role as UserRole;
+            }
+
+            return session;
+        },
+        async jwt({ token }) {
+            if (!token.sub) return token;
+
+            const existingUser = await getUserById({ userId: token.sub });
+
+            if (!existingUser) return token;
+
+            token.role = existingUser.role;
+
+            return token;
+        },
+    },
     adapter: PrismaAdapter(prisma),
     session: { strategy: "jwt" },
     ...authConfig,
