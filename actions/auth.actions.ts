ef13dd675e732fba2ec7fcf383fa29e34255850e
Kaj <kajvdm@gmail.com>
2024-01-07 14:55:07 +0100

    feat: actions for password resetting through email link

--- actions/auth.actions.ts
+++ actions/auth.actions.ts
@@ -10,14 +10,19 @@ import { getUserByEmail } from "@/actions/user.actions";
 import { signIn } from "@/auth";
 import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
 import { AuthError } from "next-auth";
-import { generateVerificationToken } from "@/lib/tokens";
-import { sendVerificationEmail } from "@/lib/mail";
+import {
+    generatePasswordResetToken,
+    generateVerificationToken,
+} from "@/lib/tokens";
+import { sendPasswordResetEmail, sendVerificationEmail } from "@/lib/mail";
+import { ResetValidation } from "@/lib/validations/reset";
+import { PasswordValidation } from "@/lib/validations/password";
 
 export const login = async (values: z.infer<typeof LoginValidation>) => {
     const validatedFields = LoginValidation.safeParse(values);
 
     if (!validatedFields.success) {
-        return { error: "Invalid fields!" };
+        return { error: "Invalid fields" };
     }
 
     const { email, password } = validatedFields.data;
@@ -65,7 +70,7 @@ export const signup = async (values: z.infer<typeof SignupValidation>) => {
     const validatedFields = SignupValidation.safeParse(values);
 
     if (!validatedFields.success) {
-        return { error: "Invalid fields!" };
+        return { error: "Invalid fields" };
     }
 
     const { email, password, name } = validatedFields.data;
@@ -124,19 +129,19 @@ export const newVerification = async (token: string) => {
     const existingToken = await getVerificationTokenByToken(token);
 
     if (!existingToken) {
-        return { error: "Token does not exist!" };
+        return { error: "Token does not exist" };
     }
 
     const hasExpired = new Date(existingToken.expires) < new Date();
 
     if (hasExpired) {
-        return { error: "Token has expired!" };
+        return { error: "Token has expired" };
     }
 
     const existingUser = await getUserByEmail({ email: existingToken.email });
 
     if (!existingUser) {
-        return { error: "Email does not exist!" };
+        return { error: "Email does not exist" };
     }
 
     await prisma.user.update({
@@ -150,4 +155,109 @@ export const newVerification = async (token: string) => {
     await prisma.verificationToken.delete({ where: { id: existingToken.id } });
 
     return { success: "Email verified" };
+};
+
+export const reset = async (values: z.infer<typeof ResetValidation>) => {
+    const validatedFields = ResetValidation.safeParse(values);
+
+    if (!validatedFields.success) {
+        return { error: "Invalid email" };
+    }
+
+    const { email } = validatedFields.data;
+
+    const existingUser = await getUserByEmail({ email });
+
+    if (!existingUser)
+        return {
+            error: "Email not found",
+        };
+
+    const passwordResetToken = await generatePasswordResetToken(email);
+    await sendPasswordResetEmail(
+        passwordResetToken.email,
+        passwordResetToken.token
+    );
+
+    return {
+        success: "Reset email sent",
+    };
+};
+
+export const getPasswordResetTokenByToken = async (token: string) => {
+    try {
+        const passwordResetToken = await prisma.passwordResetToken.findUnique({
+            where: { token },
+        });
+
+        return passwordResetToken;
+    } catch (error) {
+        return null;
+    }
+};
+
+export const getPasswordResetTokenByEmail = async (email: string) => {
+    try {
+        const passwordResetToken = await prisma.passwordResetToken.findFirst({
+            where: { email },
+        });
+
+        return passwordResetToken;
+    } catch (error) {
+        return null;
+    }
+};
+
+export const newPassword = async (
+    values: z.infer<typeof PasswordValidation>,
+    token?: string | null
+) => {
+    try {
+        if (!token) {
+            return { error: "Missing token" };
+        }
+
+        const validatedFields = PasswordValidation.safeParse(values);
+
+        if (!validatedFields.success) {
+            return { error: "Invalid fields" };
+        }
+
+        const { password } = validatedFields.data;
+
+        const existingToken = await getPasswordResetTokenByToken(token);
+
+        if (!existingToken) {
+            return { error: "Missing token" };
+        }
+
+        const hasExpired = new Date(existingToken.expires) < new Date();
+
+        if (hasExpired) {
+            return { error: "Token has expired" };
+        }
+
+        const existingUser = await getUserByEmail({
+            email: existingToken.email,
+        });
+
+        if (!existingUser) {
+            return { error: "Email does not exist" };
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        await prisma.user.update({
+            where: { id: existingUser.id },
+            data: { password: hashedPassword },
+        });
+
+        await prisma.passwordResetToken.delete({
+            where: { id: existingUser.id },
+        });
+
+        return { success: "Password updated" };
+    } catch (error) {
+        return null;
+    }
 };
