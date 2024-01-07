cf1213a2809f88e2c1ae2080adcd1f84dfbd789e
Kaj <kajvdm@gmail.com>
2024-01-07 12:01:09 +0100

    feat: initialize providers oauth flow

--- auth.config.ts
+++ auth.config.ts
@@ -1,12 +1,27 @@
 import bcrypt from "bcryptjs";
 import type { NextAuthConfig } from "next-auth";
 import Credentials from "next-auth/providers/credentials";
+import Github from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
+import Discord from "next-auth/providers/discord";
 
 import { LoginValidation } from "@/lib/validations/login";
 import { getUserByEmail } from "@/actions/user.actions";
 
 export default {
     providers: [
+        Google({
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        }),
+        Discord({
+            clientId: process.env.DISCORD_CLIENT_ID,
+            clientSecret: process.env.DISCORD_CLIENT_SECRET,
+        }),
+        Github({
+            clientId: process.env.GITHUB_CLIENT_ID,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET,
+        }),
         Credentials({
             async authorize(credentials) {
                 const validatedFields = LoginValidation.safeParse(credentials);
