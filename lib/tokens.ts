4622a1c4f088ca760112a0a3aff7fbe7e17d325e
Kaj <kajvdm@gmail.com>
2024-01-07 14:56:03 +0100

    chore: generate pwd reset token for link

--- lib/tokens.ts
+++ lib/tokens.ts
@@ -1,6 +1,34 @@
-import { getVerificationTokenByEmail } from "@/actions/auth.actions";
+import {
+    getPasswordResetTokenByEmail,
+    getVerificationTokenByEmail,
+} from "@/actions/auth.actions";
 import { prisma } from "@/utils/Utility";
 import { v4 as uuidv4 } from "uuid";
+
+export const generatePasswordResetToken = async (email: string) => {
+    const token = uuidv4();
+    const expires = new Date(new Date().getTime() + 3600 * 1000);
+
+    const existingToken = await getPasswordResetTokenByEmail(email);
+
+    if (existingToken) {
+        await prisma.passwordResetToken.delete({
+            where: {
+                id: existingToken.id,
+            },
+        });
+    }
+
+    const passwordResetToken = await prisma.passwordResetToken.create({
+        data: {
+            email,
+            token,
+            expires,
+        },
+    });
+
+    return passwordResetToken;
+};
 
 export const generateVerificationToken = async (email: string) => {
     const token = uuidv4();
