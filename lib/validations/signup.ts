9573985111174224279de31e81f1f9ed2751ee2f
Kaj <kajvdm@gmail.com>
2024-01-07 07:25:19 +0100

    feat: auth validation schemas

---
+++ lib/validations/signup.ts
@@ -0,0 +1,12 @@
+import * as z from "zod";
+
+export const SignupValidation = z.object({
+    email: z.string().email({
+        message: "Email is required",
+    }),
+    password: z.string().min(6, {
+        message: "Minimum 6 characters required",
+    }),
+    name: z.string().min(1, { message: "Name is required" }),
+    phone_number: z.number(),
+});
