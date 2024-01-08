9e56a18f9182ce6de28bc7b8b7cd5dde6bee80da
Kaj <kajvdm@gmail.com>
2024-01-08 08:55:54 +0100

    fix: populate user button with actual image data and add logout button to dropdown

--- components/user/UserButton.tsx
+++ components/user/UserButton.tsx
@@ -2,20 +2,35 @@ import React from "react";
 import {
     DropdownMenu,
     DropdownMenuTrigger,
+    DropdownMenuContent,
+    DropdownMenuItem,
 } from "@/components/ui/dropdown-menu";
 import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
 import { FaUser } from "react-icons/fa";
+import { useCurrentUser } from "@/lib/hooks/useCurrentUser";
+import { LogoutButton } from "@/components/auth/LogoutButton";
+import { ExitIcon } from "@radix-ui/react-icons";
+
 const UserButton = () => {
+    const user = useCurrentUser();
     return (
         <DropdownMenu>
             <DropdownMenuTrigger>
                 <Avatar>
-                    <AvatarImage src="" />
+                    <AvatarImage src={user?.image || ""} />
                     <AvatarFallback className="bg-white">
                         <FaUser />
                     </AvatarFallback>
                 </Avatar>
             </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-40" align="end">
+                <LogoutButton>
+                    <DropdownMenuItem>
+                        <ExitIcon className="h4 w-4 mr-2" />
+                        Logout
+                    </DropdownMenuItem>
+                </LogoutButton>
+            </DropdownMenuContent>
         </DropdownMenu>
     );
 };
