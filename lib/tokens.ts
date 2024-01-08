1c295e59d0522dabed33f100507dad2a4df8792b
Kaj <kajvdm@gmail.com>
2024-01-08 07:04:44 +0100

    feat: generate token for 2fa

--- lib/tokens.ts
+++ lib/tokens.ts
@@ -1,9 +1,38 @@
 import {
     getPasswordResetTokenByEmail,
+    getTwoFactorTokenByEmail,
     getVerificationTokenByEmail,
 } from "@/actions/auth.actions";
 import { prisma } from "@/utils/Utility";
+
 import { v4 as uuidv4 } from "uuid";
+import crypto from "crypto";
+
+export const generateTwoFactorToken = async (email: string) => {
+    const token = crypto.randomInt(100_000, 1_000_000).toString();
+    //  TODO: Change to 15 minutes
+    const expires = new Date(new Date().getTime() + 3600 * 1000);
+
+    const existingToken = await getTwoFactorTokenByEmail(email);
+
+    if (existingToken) {
+        await prisma.twoFactorToken.delete({
+            where: {
+                id: existingToken.id,
+            },
+        });
+    }
+
+    const twoFactorToken = await prisma.twoFactorToken.create({
+        data: {
+            email,
+            token,
+            expires,
+        },
+    });
+
+    return twoFactorToken;
+};
 
 export const generatePasswordResetToken = async (email: string) => {
     const token = uuidv4();
