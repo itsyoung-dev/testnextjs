7b4e1d7267ab0250bd0f5875bc30501bdcb91f99
Kaj <kajvdm@gmail.com>
2024-01-08 14:12:36 +0100

    fix: add 2fa state to session

--- auth.ts
+++ auth.ts
@@ -52,6 +52,11 @@ export const {
                 session.user.role = token.role as UserRole;
             }
 
+            if (session.user) {
+                session.user.isTwoFactorEnabled =
+                    token.isTwoFactorEnabled as boolean;
+            }
+
             return session;
         },
         async jwt({ token }) {
@@ -62,6 +67,7 @@ export const {
             if (!existingUser) return token;
 
             token.role = existingUser.role;
+            token.isTwoFactorEnabled = existingUser.isTwoFactorEnabled;
 
             return token;
         },
