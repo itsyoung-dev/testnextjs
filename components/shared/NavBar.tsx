cbac4e313c59db9c6847d96134487e0eef4fd01f
Kaj <kajvdm@gmail.com>
2024-01-08 08:26:56 +0100

    fix: show custom user button with logged in state

--- components/shared/NavBar.tsx
+++ components/shared/NavBar.tsx
@@ -1,13 +1,15 @@
 "use client";
 import { NavLinks } from "@/constants/constants";
-import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
-// import UserButton from "@/components/user/UserButton";
+import UserButton from "@/components/user/UserButton";
+import { useCurrentUser } from "@/lib/hooks/useCurrentUser";
 
 const NavBar = () => {
     const [showNavbar, setShowNavbar] = useState(true);
+    const user = useCurrentUser();
+
     useEffect(() => {
         const handleScroll = () => {
             if (window.scrollY > 0) {
@@ -68,7 +70,7 @@ const NavBar = () => {
                                     </a>
                                 </React.Fragment>
                             ))}
-                            <SignedIn>
+                            {user && (
                                 <div className="hidden md:block">
                                     <a
                                         href="/login"
@@ -77,45 +79,51 @@ const NavBar = () => {
                                         Contact
                                     </a>
                                 </div>
-                            </SignedIn>
+                            )}
                         </div>
                     </div>
                     <div className="flex items-center gap-x-5">
-                        <SignedOut>
-                            <div className="hidden md:block">
+                        {!user && (
+                            <>
+                                <div className="hidden md:block">
+                                    <a
+                                        href="/contact"
+                                        className="inline-block rounded-lg px-2 py-1 text-sm text-secondary-gray hover:text-primary-white/[88%] duration-200"
+                                    >
+                                        Contact
+                                    </a>
+                                </div>
+                                <div className="hidden md:block">
+                                    <a
+                                        href="/login"
+                                        className="flex items-center rounded-md px-4 py-[4px] text-sm text-primary-white duration-200 bg-white/10 border border-white/10 hover:border-white/20"
+                                    >
+                                        <span className="pt-[2px]">Log in</span>
+                                    </a>
+                                </div>
                                 <a
-                                    href="/contact"
-                                    className="inline-block rounded-lg px-2 py-1 text-sm text-secondary-gray hover:text-primary-white/[88%] duration-200"
+                                    href="/signup"
+                                    className="flex items-center justify-center rounded-md py-[4px] px-4 text-sm font-normal focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 bg-primary-blurple text-primary-white hover:opacity-80 duration-200"
                                 >
-                                    Contact
+                                    <span className="pt-[2px]">Sign up</span>
                                 </a>
-                            </div>
-                            <div className="hidden md:block">
-                                <a
-                                    href="/login"
-                                    className="flex items-center rounded-md px-4 py-[4px] text-sm text-primary-white duration-200 bg-white/10 border border-white/10 hover:border-white/20"
-                                >
-                                    <span className="pt-[2px]">Log in</span>
-                                </a>
-                            </div>
-                            <a
-                                href="/signup"
-                                className="flex items-center justify-center rounded-md py-[4px] px-4 text-sm font-normal focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 bg-primary-blurple text-primary-white hover:opacity-80 duration-200"
-                            >
-                                <span className="pt-[2px]">Sign up</span>
-                            </a>
-                        </SignedOut>
-                        <SignedIn>
-                            <div className="mr-1">
-                                <a
-                                    href="/dashboard"
-                                    className="flex items-end text-center bg-white/10 border border-white/10 rounded-md px-3 py-[4px] text-sm text-primary-white hover:text-primary-white/[88%] hover:border-white/20 duration-200"
-                                >
-                                    <span className="pt-[2px]">Dashboard</span>
-                                </a>
-                            </div>
-                            <UserButton />
-                        </SignedIn>
+                            </>
+                        )}
+                        {user && (
+                            <>
+                                <div className="mr-1">
+                                    <a
+                                        href="/dashboard"
+                                        className="flex items-end text-center bg-white/10 border border-white/10 rounded-md px-3 py-[4px] text-sm text-primary-white hover:text-primary-white/[88%] hover:border-white/20 duration-200"
+                                    >
+                                        <span className="pt-[2px]">
+                                            Dashboard
+                                        </span>
+                                    </a>
+                                </div>
+                                <UserButton />
+                            </>
+                        )}
                         <div className="-mr-1 md:hidden"></div>
                     </div>
                 </nav>
