67619658c450404695bda7b2939765847d9f82d8
Kaj <kajvdm@gmail.com>
2024-01-09 14:15:09 +0100

    feat: also update settings on server side and validate email or password

--- actions/dashboard.actions.ts
+++ actions/dashboard.actions.ts
@@ -1,10 +1,15 @@
 "use server";
 
 import * as z from "zod";
+import bcrypt from "bcryptjs";
+
+import { update } from "@/auth";
 import { currentUser } from "@/lib/helpers/auth";
 import { SettingsValidation } from "@/lib/validations/settings";
-import { getUserById } from "./user.actions";
+import { getUserByEmail, getUserById } from "./user.actions";
 import { prisma } from "@/lib/utils";
+import { generateVerificationToken } from "@/lib/helpers/tokens";
+import { sendVerificationEmail } from "@/lib/helpers/mail";
 
 export const settings = async (values: z.infer<typeof SettingsValidation>) => {
     const user = await currentUser();
@@ -19,12 +24,59 @@ export const settings = async (values: z.infer<typeof SettingsValidation>) => {
         return { error: "Unauthorized" };
     }
 
-    await prisma.user.update({
+    if (user.isOAuth) {
+        values.email = undefined;
+        values.password = undefined;
+        values.newPassword = undefined;
+        values.isTwoFactorEnabled = undefined;
+    }
+
+    if (values.email && values.email !== user.email) {
+        const existingUser = await getUserByEmail({ email: values.email });
+
+        if (existingUser && existingUser.id !== user.id) {
+            return { error: "Email already in use!" };
+        }
+
+        const verificationToken = await generateVerificationToken(values.email);
+        await sendVerificationEmail(
+            verificationToken.email,
+            verificationToken.token
+        );
+
+        return { success: "Verification email sent!" };
+    }
+
+    if (values.password && values.newPassword && dbUser.password) {
+        const passwordsMatch = await bcrypt.compare(
+            values.password,
+            dbUser.password
+        );
+
+        if (!passwordsMatch) {
+            return { error: "Incorrect password!" };
+        }
+
+        const hashedPassword = await bcrypt.hash(values.newPassword, 10);
+        values.password = hashedPassword;
+        values.newPassword = undefined;
+    }
+
+    const updatedUser = await prisma.user.update({
         where: { id: dbUser.id },
         data: {
             ...values,
         },
     });
 
-    return { success: "Settings updated" };
+    update({
+        user: {
+            name: updatedUser.name,
+            email: updatedUser.email,
+            isTwoFactorEnabled: updatedUser.isTwoFactorEnabled,
+            role: updatedUser.role,
+        },
+    });
+
+    return { success: "Settings Updated!" };
 };
