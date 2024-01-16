c1afd3875ef8797d75b61197f8cd77a2259940f8
Kajgit <kajvdm@gmail.com>
2024-01-16 12:39:41 +0100

    style: move sidebar outside main and add left padding

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -50,8 +50,8 @@ export default async function RootLayout({
                 <body className={`${inter.className}`}>
                     <Topbar />
 
-                    <main className="flex flex-row">
                         <Sidebar />
+                    <main className="ml-72">
                         <section className="flex min-h-screen flex-1 flex-col items-center bg-dark-1 px-6 pb-10 pt-28 max-md:pb-32 sm:px-10">
                             <div className="w-full">{children}</div>
                         </section>
