428677f64fae0c4044a08a7736370ff74d8947b3
Kaj <kajvdm@gmail.com>
2024-01-16 19:45:47 +0100

    feat: index searchable pages for auto complete function

---
+++ constants/search.ts
@@ -0,0 +1,18 @@
+export const indexxedSearchPages = [
+    {
+        pageName: "Dashboard",
+        href: "/dashboard",
+    },
+    {
+        pageName: "Statistics",
+        href: "/statistics",
+    },
+    {
+        pageName: "Invoices",
+        href: "/invoices",
+    },
+    {
+        pageName: "Settings",
+        href: "/settings",
+    },
+];
