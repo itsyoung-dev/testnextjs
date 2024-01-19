1a7b7690f267811baeb261293377e9870c5b9d5b
Kajgit <kajvdm@gmail.com>
2024-01-19 19:58:15 +0100

    fix: properly check typegate for result

--- components/auth/TypeGate.tsx
+++ components/auth/TypeGate.tsx
@@ -1,12 +1,12 @@
 interface TypeGateProps {
     children: React.ReactNode;
-    allowedType: boolean;
+    allowedType: boolean | undefined | null;
 }
 export const TypeGate = ({ children, allowedType }: TypeGateProps) => {
-
-    if (!allowedType) {
+    if (allowedType) {
         return <div></div>;
     }
 
     return <>{children}</>;
 };
+
