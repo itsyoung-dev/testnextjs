d73bdfb1c37b2a11d45c1c158774eef84379d445
Kaj <kajvdm@gmail.com>
2024-01-23 17:41:30 +0100

    feat: add type gate for certain dashboard statistics

--- components/shared/dashboard/Sidebar.tsx
+++ components/shared/dashboard/Sidebar.tsx
@@ -21,6 +21,8 @@ import { currentUser } from "@/lib/helpers/auth";
 
 const Sidebar = async () => {
     const user = await currentUser();
+
+    console.log(user?.isAffiliate);
     return (
         <div className="bg-[#0c0c0c] w-72 border-r border-white/10 pt-8 self-start fixed bottom-0 left-0 h-[calc(100vh-75px)]  max-md:hidden z-20 overflow-auto">
             <div className="flex flex-col justify-between h-full">
@@ -31,13 +33,16 @@ const Sidebar = async () => {
                             href="/dashboard"
                             label="Home"
                         />
-                        <SidebarLink
-                            icon={
-                                <CursorArrowRaysIcon className="w-5 text-[#ffffffdb]" />
-                            }
-                            href="/dashboard/affiliates"
-                            label="Affiliate Program"
-                        />
+
+                        <TypeGate allowedType={user?.isAffiliate}>
+                            <SidebarLink
+                                icon={
+                                    <CursorArrowRaysIcon className="w-5 text-[#ffffffdb]" />
+                                }
+                                href="/dashboard/affiliates"
+                                label="Affiliate Program"
+                            />
+                        </TypeGate>
                         <SidebarLink
                             icon={
                                 <ChartPieIcon className="w-5 text-[#ffffffdb]" />
@@ -94,28 +99,33 @@ const Sidebar = async () => {
                             />
                         </LogoutButton>
                     </SidebarSection>
-                    <RoleGate allowedRole="ADMIN" >
-                    <SidebarSection header="ADMINISTRATOR TOOLS" topDivider={true}>
-                        <SidebarLink
-                            icon={<UserIcon className="w-5 text-[#ffffffdb]" />}
-                            href="/"
-                            label="Manage users"
-                        />
-                        <SidebarLink
-                            icon={
-                                <SwatchIcon className="w-5 text-[#ffffffdb]" />
-                            }
-                            href="/"
-                            label="Manage services"
-                        />
-                        <SidebarLink
-                            icon={
-                                <BanknotesIcon className="w-5 text-[#ffffffdb]" />
-                            }
-                            href="/"
-                            label="Manage payments"
-                        />
-                    </SidebarSection>
+                    <RoleGate allowedRole="ADMIN">
+                        <SidebarSection
+                            header="ADMINISTRATOR TOOLS"
+                            topDivider={true}
+                        >
+                            <SidebarLink
+                                icon={
+                                    <UserIcon className="w-5 text-[#ffffffdb]" />
+                                }
+                                href="/"
+                                label="Manage users"
+                            />
+                            <SidebarLink
+                                icon={
+                                    <SwatchIcon className="w-5 text-[#ffffffdb]" />
+                                }
+                                href="/"
+                                label="Manage services"
+                            />
+                            <SidebarLink
+                                icon={
+                                    <BanknotesIcon className="w-5 text-[#ffffffdb]" />
+                                }
+                                href="/"
+                                label="Manage payments"
+                            />
+                        </SidebarSection>
                     </RoleGate>
                 </div>
                 <div className="pl-5 py-5 h-full flex justify-end flex-col font-medium">
